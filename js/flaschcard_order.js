5f5d7b8341711b2e7f12d52711cfa85e8151979e
Szymon <szymonadamowicz8@gmail.com>
2023-10-18 09:09:56 +0200

    fix on flashcard order and on visibility of answer

--- js/flaschcard_order.js
+++ js/flaschcard_order.js
@@ -1,7 +1,8 @@
 var flashcards;
 var currentQuestionIndex = 0;
+var answerVisible = false;
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   var categorySpan = document.getElementById("categorySpan");
 
   var title = localStorage.getItem("title");
@@ -27,76 +28,69 @@ document.addEventListener("DOMContentLoaded", function() {
       })
       .catch(error => console.error('Błąd pobierania danych z pliku JSON:', error));
   }
-  window.addEventListener("load", fetchFlashcards(category));
+  window.addEventListener("load", function() {
+    fetchFlashcards(category);
+  });
 });
-
 
 function showQuestion() {
   var questionContainer = document.getElementById("question-container");
   var answerContainer = document.getElementById("answer-container");
+  var nextQuestionButton = document.getElementById("nextQuestionButton");
 
-  if (currentQuestionIndex == 0) {
+  if (currentQuestionIndex === 0) {
     var prevQuestionButton = document.getElementById("prevQuestionButton");
     prevQuestionButton.style.opacity = 0.1;
-    prevQuestionButton.style.pointerEvents = "none"
-
+    prevQuestionButton.style.pointerEvents = "none";
   } else {
     var prevQuestionButton = document.getElementById("prevQuestionButton");
     prevQuestionButton.style.opacity = 1;
-    prevQuestionButton.style.display = "inline-block";
-    prevQuestionButton.style.pointerEvents = "unset"
+    prevQuestionButton.style.pointerEvents = "unset";
   }
 
   if (currentQuestionIndex < flashcards.length) {
     var randomQuestion = flashcards[currentQuestionIndex].question;
     var randomAnswer = flashcards[currentQuestionIndex].answer;
 
-    questionContainer.innerHTML = randomQuestion;
-    nextQuestionButton.style.opacity = 1;
-    nextQuestionButton.style.pointerEvents = "unset";
-    questionContainer.style.pointerEvents = "unset";
-    questionContainer.style.display = "block";
-    answerContainer.style.display = "none";
-
-    questionContainer.onclick = function () {
+    if (!answerVisible) {
+      questionContainer.innerHTML = randomQuestion;
+      nextQuestionButton.style.opacity = 1;
+      nextQuestionButton.style.pointerEvents = "unset";
+      questionContainer.style.pointerEvents = "unset";
+      questionContainer.style.display = "block";
+      answerContainer.style.display = "none";
+    } else {
+      questionContainer.style.display = "none";
       answerContainer.innerHTML = randomAnswer;
       answerContainer.style.display = "block";
-    };
-
+    }
+    
   } else {
     questionContainer.innerHTML = "Nie ma więcej pytań.";
     questionContainer.style.pointerEvents = "none";
     nextQuestionButton.style.opacity = 0.1;
     nextQuestionButton.style.pointerEvents = "none";
-    answerContainer.style.display = "none"; // Dodaj tę linię, aby ukryć odpowiedź
+    answerContainer.style.display = "none";
   }
   updateProgressBar();
 }
 
 function updateProgressBar() {
   const progressBar = document.getElementById("progress-bar");
-  const progress = ((currentQuestionIndex ) / flashcards.length) * 100; 
-  progressBar.style.width = progress + "%"; 
-  document.getElementById("abc").innerHTML ="Ukończono "   + (currentQuestionIndex) + '/' + flashcards.length;
-
+  const progress = (currentQuestionIndex / flashcards.length) * 100;
+  progressBar.style.width = progress + "%";
+  document.getElementById("abc").innerHTML = "Ukończono " + currentQuestionIndex + '/' + flashcards.length;
 }
 
-var answerVisible = false;
-
 function toggleAnswer() {
-  var answerContainer = document.getElementById("answer-container");
-  if (!answerVisible) {
-    answerContainer.style.display = "none";
-}}
-
-var answerContainer = document.getElementById("answer-container");
-if (answerContainer) {
-  answerContainer.addEventListener("click", toggleAnswer);
+  answerVisible = !answerVisible; 
+  showQuestion();
 }
 
 function nextQuestion() {
   if (currentQuestionIndex < flashcards.length) {
     currentQuestionIndex++;
+    answerVisible = false; 
     showQuestion();
     updateProgressBar();
   }
@@ -105,6 +99,7 @@ function nextQuestion() {
 function prevQuestion() {
   if (currentQuestionIndex > 0) {
     currentQuestionIndex--;
+    answerVisible = false;
     showQuestion();
     updateProgressBar();
   }
@@ -117,3 +112,4 @@ function redirectToIndex() {
 document.getElementById("prevQuestionButton").addEventListener("click", prevQuestion);
 document.getElementById("nextQuestionButton").addEventListener("click", nextQuestion);
 document.getElementById("chooseCategoryButton").addEventListener("click", redirectToIndex);
+document.getElementById("question-container").addEventListener("click", toggleAnswer);
